c468ce83f2b34fbc81dc5627899ca1c8a64d5301
fernandodebsas <fernandodebsas@hotmail.com>
2022-05-30 23:38:01 -0300

    Initial commit

---
+++ src/main/java/com/portfoli0/fv/FvApplication.java
@@ -0,0 +1,13 @@
+package com.portfoli0.fv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FvApplication.class, args);
+	}
+
+}
